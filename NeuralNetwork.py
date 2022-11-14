688d4bf4eedbe262dc4d2355d175c609e7567f56
Даниил Никоненко <alfa_fk@mail.ru>
2022-11-14 21:58:24 +0300

    передача данных в нейронную сеть

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -15,23 +15,19 @@ class NeuralNetwork:
         df.head()
         abalone_features = train.copy()
         abalone_labels = abalone_features.pop('y')
-        abalone_test = test.copy()
-        abalone_y_test = abalone_test.pop('y')
+        self.abalone_test = test.copy()
+        abalone_y_test = self.abalone_test.pop('y')
+        #print(self.abalone_test)
         self.model = Sequential()
-        self.model.add(Dense(56, input_dim=9, activation='relu'))
+        self.model.add(Dense(56, input_dim=10, activation='relu'))
         self.model.add(Dense(56, activation='relu'))
         self.model.add(Dense(1, activation='relu'))
         self.model.compile(optimizer='adam', loss='mae') 
         self.losses = self.model.fit(abalone_features, abalone_labels, epochs=30, )
-        V = self.model.predict(abalone_test).round()
-    def SaveModel(self , path):
-        self.model.save(path)
-
+       # V = self.model.predict(self.abalone_test).round()
+    def SaveModel(self , path): self.model.save(path)
     def LoadModel(self , path): self.model = keras.models.load_model(path)
-
     def NeuralWork(self , data): return self.model.predict(data)
-    def Normaliation(self): 
-        return 0
     def ShowNeuralData(self):
         fig = plt.figure()
         ax = fig.add_subplot()
@@ -57,20 +53,29 @@ def get_request():
   #  print(post_response.content)
 
 NN = NeuralNetwork()
+#NN.Learn("D:\\GIT_RPS\LoLNeural\\data\\N_YDataset.csv")
+#NN.SaveModel("D:\\GIT_RPS\LoLNeural\\model")
 NN.LoadModel("D:\\GIT_RPS\LoLNeural\\model")
+
+
+dd = {'kill': 100.0, 'abilityPower': 5.0, 'armor': 13.149999999999999, 'attackDamage': -22.740000000000002, 'attackSpeed': -0.19700000000000006, 'healthMax': 27.0, 'lifesteal': 0.0, 'magicResist': 2.1999999999999957, 'movementSpeed': -5.0, 'powerMax': -379.0}
 
 while(True):
     DATA = []
     data = json.loads(get_request())
     d1 = dict(list(data.items())[:len(data)-3])
-    for key, value in d1.items(): DATA.append(value)
-    df=pd.DataFrame(DATA)
+    for key, value in dd.items(): DATA.append(value)
+    df=pd.DataFrame(DATA )
+    #df.head()
+    #df.transpose()
     normalized_df=(df-df.mean())/df.std()
     #print(df[0].to_numpy())
-    print(d1)
-    #print(NN.NeuralWork(normalized_df.to_numpy()))
-
-
+    #print(d1.to_numpy())
+    #test_input = np.random.random((1, 10))
+    #print(test_input)
+    print(np.transpose(normalized_df))
+    #print(np.transpose(normalized_df))
+    print(NN.NeuralWork(np.transpose(normalized_df)))
 
 
 
