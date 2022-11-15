eb593f7e1c3d4c7b3c4a7522097b5ae29c61b18c
Даниил Никоненко <alfa_fk@mail.ru>
2022-11-15 11:21:41 +0300

    Передача результатов на сервер

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,11 +7,6 @@ from matplotlib import pyplot as plt
 from tensorflow import keras
 import requests
 import json
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import SpectralClustering
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.metrics.pairwise import pairwise_distances
 
 class NeuralNetwork:
     def Learn(self, filename):
@@ -22,16 +17,15 @@ class NeuralNetwork:
         abalone_labels = abalone_features.pop('y')
         self.abalone_test = test.copy()
         abalone_y_test = self.abalone_test.pop('y')
-        print(self.abalone_test)
         self.model = Sequential()
         self.model.trainable = True
         self.model.add(Dense(70, input_dim=10, activation='relu'))
         self.model.add(Dense(70, activation='relu'))
         self.model.add(Dense(1, activation='relu'))
         self.model.compile(optimizer='adam', loss='mae' , metrics=['accuracy']) 
-        self.losses = self.model.fit(abalone_features, abalone_labels, epochs=20, )
+        self.losses = self.model.fit(abalone_features, abalone_labels, epochs=30, )
         V = self.model.predict(self.abalone_test).round()
-        print(V)
+       # print(V)
     def SaveModel(self , path): self.model.save(path)
     def LoadModel(self , path): 
         self.model = keras.models.load_model(path)
@@ -46,8 +40,27 @@ class NeuralNetwork:
         plt.plot(self.losses.history["loss"])
         plt.show()
 
-
-    #def __init__(self, filename):
+def get_request(_url):
+    get_param = {'index':'10'}
+    get_response = requests.get(url=_url , params=get_param)
+    print(get_response)
+    return get_response.content
+def post_request(_url , data):
+    post_param = {'winChance':data}
+    post_response = requests.post(url=_url , data=post_param)
+def start_request(url):
+    while(True):
+        DATA = []
+        data = json.loads(get_request(url))
+        d1 = dict(list(data.items())[:len(data)-5])
+        for key, value in d1.items(): DATA.append(value)
+        df=pd.DataFrame(DATA )
+        normalized_df=(np.transpose(df)-DFD.mean().to_numpy())/DFD.std().to_numpy()
+        accuary = str(NN.NeuralWork(df)[0])
+        if(accuary == '0'):  accuary = 'draw'
+        if(accuary == '1'):  accuary = 'lose'
+        if(accuary == '2'):  accuary = 'win'
+        post_request(url , accuary)
 
 
 
@@ -55,89 +68,9 @@ Df = pd.read_csv("D:\\GIT_RPS\LoLNeural\\data\\YDataset.csv")
 Df = Df.drop("y", axis = 1)
 x = Df.values
 DFD = pd.DataFrame(x)
-print(DFD)
-
-def get_request():
-    get_param = {'index':'10'}
-    get_response = requests.get(url="http://25.47.99.103:8080/local/status" , params=get_param)
-    print(get_response)
-    return get_response.content
-def post_request(data):
-    post_param = {'winChance':data}
-    post_response = requests.post(url="http://25.47.99.103:8080/local/status" , data=post_param)
-
 NN = NeuralNetwork()
-NN.Learn("D:\\GIT_RPS\LoLNeural\\data\\N_YDataset.csv")
-#NN.LoadModel("D:\\GIT_RPS\LoLNeural\\model")
+#N.Learn("D:\\GIT_RPS\LoLNeural\\data\\N_YDataset.csv")
+NN.LoadModel("D:\\GIT_RPS\LoLNeural\\model")
 #NN.SaveModel("D:\\GIT_RPS\LoLNeural\\model")
-
-
-
-dd = {'kill': 0.6842105263157894, 'abilityPower': 0.56565402962591, 'armor': 0.5770452740270056, 'attackDamage': 0.5154098360655738, 'attackSpeed': 0.7226289800783245, 'healthMax': 0.6219512195121952, 'lifesteal': 0.5788216560509554, 'magicResist': 0.5779100037188546, 'movementSpeed': 0.5779100037188546, 'powerMax': 0.5678027280671758}
-min_max_scaler = preprocessing.MinMaxScaler()
-while(True):
-    DATA = []
-    data = json.loads(get_request())
-    d1 = dict(list(data.items())[:len(data)-4])
-    for key, value in dd.items(): DATA.append(value)
-    df=pd.DataFrame(DATA )
-    x_scaled = min_max_scaler.fit_transform(DFD.values)
-    normalized_df=(np.transpose(df)-np.transpose(DFD.mean()))/np.transpose(DFD.std())
-    print(np.transpose(df))
-    #accuary = str(NN.NeuralWork(df)[0])
-    print(NN.NeuralWork(np.transpose(df)))
-   # post_request(accuary)
-
-
-
-"""
-NN = NeuralNetwork()
-NN.Learn("D:\\GIT_RPS\LoLNeural\\data\\N_YDataset.csv")
-NN.ShowNeuralData()
-NN.SaveModel("D:\\GIT_RPS\LoLNeural\\model")
-"""
-
-"""
-filename = "C:\\neural\\N_YDataset.csv"
-df = pd.read_csv(filename)
-train, validate, test = np.split(df.sample(frac=1, random_state=42), [int(.6*len(df)), int(.8*len(df))])
-df.head()
-abalone_features = train.copy()
-abalone_labels = abalone_features.pop('y')
-
-abalone_test = test.copy()
-abalone_y_test = abalone_test.pop('y')
-
-np.savetxt('TYdata.txt', abalone_y_test, delimiter=',')
-model = Sequential()
-model.add(Dense(56, input_dim=28, activation='relu'))
-model.add(Dense(56, activation='relu'))
-model.add(Dense(1, activation='relu'))
-model.compile(optimizer='adam', loss='mae') 
-losses = model.fit(abalone_features, abalone_labels, epochs=30, )
-V = model.predict(abalone_test).round()
-
-print(V[0])
-np.savetxt('Ydata.txt', V, delimiter=',')
-Vnp = np.array(V)
-abalone_y_testnp = abalone_y_test.to_numpy()
-er = 0
-for i in range(0,len(Vnp)):
-    if(int(Vnp[i]) != abalone_y_testnp[i] ): er +=1
-print("error is:" , er/len(Vnp) * 100)
-
-fig = plt.figure()
-ax = fig.add_subplot()
-fig.subplots_adjust(top=0.85)
-
-# Set titles for the figure and the subplot respectively
-ax.set_title('обучение нейронной сети' , fontsize=14)
-
-ax.set_xlabel('эпохи' , fontsize=14)
-ax.set_ylabel('ошибка' , fontsize=14)
-
-print(losses.history)
-plt.plot(losses.history["loss"])
-plt.show()
-"""
-#plt.plot(losses.history['val_acc'])
+#dd = {'kill': 13, 'abilityPower': 178, 'armor': 193, 'attackDamage': 104, 'attackSpeed': 79, 'healthMax': 2126, 'lifesteal': 31, 'magicResist': 115, 'movementSpeed': 41, 'powerMax':1688}
+start_request("http://25.47.99.103:8080/local/status")
---
+++ n.py
@@ -0,0 +1,154 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn.cluster import KMeans
+import json
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import SpectralClustering
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+'''
+def distvec(vec_x , vec_y):
+ dist = 0
+ for i in range(0 , len(vec_x)-1):
+        dist += (vec_y[i] - vec_x[i])**2
+ return dist**0.5
+
+
+def find_max_vec(vecdata , ydata , center):
+        max_vec_len = [0,0,0]
+        for i in range(0 , len(ydata)):
+                if(max_vec_len[int(ydata[i])] < vecdata[i]): max_vec_len[int(ydata[i])] = vecdata[i]
+        return max_vec_len
+        
+def find_procent(max_vec_len ,ydata, vecdata , center):
+        procent = []
+        for i in range(0 , len(ydata)):
+
+'''
+
+
+fig = plt.figure(figsize=(15, 8))
+ax1 = fig.add_subplot(121)
+colnames = ["time",
+        "kill",
+        "death",
+        "assist",
+        "abilityHaste",
+        "abilityPower",
+        "armor",
+        "armorPen",
+        "armorPenPercent",
+        "attackDamage",
+        "attackSpeed",
+        "bonusArmorPenPercent",
+        "bonusMagicPenPercent",
+        "ccReduction",
+        "cooldownReduction",
+        "health",
+        "healthMax",
+        "healthRegen",
+        "lifesteal",
+        "magicPen",
+        "magicPenPercent",
+        "magicResist",
+        "movementSpeed",
+        "omnivamp",
+        "physicalVamp",
+        "power",
+        "powerMax",
+        "powerRegen",
+        "spellVamp"]
+p = "C:\\neural\\dataset.csv"
+Df = pd.read_csv(p,names=colnames)
+
+
+df = Df.drop("time", axis = 1)
+df = df.drop("death", axis = 1)
+df = df.drop("assist", axis = 1)
+df = df.drop("abilityHaste", axis = 1)
+df = df.drop("armorPen", axis = 1)
+df = df.drop("armorPenPercent", axis = 1)
+df = df.drop("bonusArmorPenPercent", axis = 1)
+df = df.drop("bonusMagicPenPercent", axis = 1)
+df = df.drop("ccReduction", axis = 1)
+df = df.drop("cooldownReduction", axis = 1)
+df = df.drop("health", axis = 1)
+df = df.drop("healthRegen", axis = 1)
+df = df.drop("magicPen", axis = 1)
+df = df.drop("magicPenPercent", axis = 1)
+df = df.drop("omnivamp", axis = 1)
+df = df.drop("physicalVamp", axis = 1)
+df = df.drop("power", axis = 1)
+df = df.drop("powerRegen", axis = 1)
+df = df.drop("spellVamp", axis = 1)
+
+print(df)
+normalized_df=(df-df.mean())/df.std()
+X = df.values[:, :]
+X = np.nan_to_num(normalized_df)
+clusDataSet = StandardScaler().fit_transform(X)
+clusterNum = 3
+kMeans = KMeans(init="k-means++", n_clusters=clusterNum, n_init=12)
+kMeans.fit(X)
+labels = kMeans.fit_predict(X)
+#df[TypeClaster] = labels
+"""
+c_x = kMeans.cluster_centers_[:,14]
+c_y = kMeans.cluster_centers_[:,15]
+print(kMeans.cluster_centers_[0])
+
+
+ax1.scatter(X[:,14], X[:,15], c=labels.astype(np.float), alpha=0.5)
+ax1.scatter(c_x, c_y, s = 150, c = ["red"], marker = '*', label = 'Centroids')
+ax1.set(xlabel='armor', ylabel='magic Resist', title="Clustering data")
+"""
+Dfy = pd.read_csv(p , names=colnames)
+
+
+
+Dfy = Dfy.drop("time", axis = 1)
+Dfy = Dfy.drop("death", axis = 1)
+Dfy = Dfy.drop("assist", axis = 1)
+Dfy = Dfy.drop("abilityHaste", axis = 1)
+Dfy = Dfy.drop("armorPen", axis = 1)
+Dfy = Dfy.drop("armorPenPercent", axis = 1)
+Dfy = Dfy.drop("bonusArmorPenPercent", axis = 1)
+Dfy = Dfy.drop("bonusMagicPenPercent", axis = 1)
+Dfy = Dfy.drop("ccReduction", axis = 1)
+Dfy = Dfy.drop("cooldownReduction", axis = 1)
+Dfy = Dfy.drop("health", axis = 1)
+Dfy = Dfy.drop("healthRegen", axis = 1)
+Dfy = Dfy.drop("magicPen", axis = 1)
+Dfy = Dfy.drop("magicPenPercent", axis = 1)
+Dfy = Dfy.drop("omnivamp", axis = 1)
+Dfy = Dfy.drop("physicalVamp", axis = 1)
+Dfy = Dfy.drop("power", axis = 1)
+Dfy = Dfy.drop("powerRegen", axis = 1)
+Dfy = Dfy.drop("spellVamp", axis = 1)
+
+
+
+x = df.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+normalized_dfy = pd.DataFrame(x_scaled)
+
+#normalized_dfy=float((Dfy-Dfy.mean())/Dfy.std())
+normalized_dfy['y'] = pd.Series(labels, index=normalized_dfy.index)
+Dfy['y'] = pd.Series(labels, index=Dfy.index)
+
+#print(normalized_dfy.sort_values(['y'], ascending=[False])['y'])
+'''
+Dists = []
+vecdist = normalized_dfy
+for index, row in vecdist.iterrows(): Dists.append(distvec(row , kMeans.cluster_centers_[int(row['y'])]))
+maxlenvec = find_max_vec(Dists ,labels ,kMeans.cluster_centers_)
+print(maxlenvec)
+'''
+Dfy.to_csv("YDataset.csv", encoding='utf-8', index=False)
+normalized_dfy.to_csv("N_YDataset.csv", encoding='utf-8', index=False)
+#plt.show()
